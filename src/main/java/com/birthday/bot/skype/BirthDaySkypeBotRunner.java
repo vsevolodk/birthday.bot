3e438650e4dbcd5ea13cba27f1355d2eb354d3a5
vsevolodk <vsevolodkaymashnikov@gmail.com>
2016-03-14 02:14:47 +0300

    added sync chats logic. This need when files is missed. You can interactive restore chat states.

--- src/main/java/com/birthday/bot/skype/BirthDaySkypeBotRunner.java
+++ src/main/java/com/birthday/bot/skype/BirthDaySkypeBotRunner.java
@@ -2,12 +2,15 @@ package com.birthday.bot.skype;
 
 import com.birthday.bot.skype.bot.BirthdayChatCreator;
 import com.birthday.bot.skype.bot.listener.ListenerRegister;
+import com.birthday.bot.skype.chat.ChatForBDay;
 import com.birthday.bot.skype.contact.ContactRepository;
 import com.birthday.bot.skype.contact.ContactWithBDay;
 import com.birthday.bot.skype.settings.BirthdayBot;
 import com.birthday.bot.skype.settings.loader.BirthdayBotSettings;
 import com.samczsun.skype4j.Skype;
 import com.samczsun.skype4j.SkypeBuilder;
+import com.samczsun.skype4j.chat.Chat;
+import com.samczsun.skype4j.chat.GroupChat;
 import com.samczsun.skype4j.exceptions.*;
 import com.birthday.bot.skype.bot.PingRunner;
 import com.birthday.bot.skype.chat.ChatRepository;
@@ -15,7 +18,7 @@ import com.birthday.bot.skype.holder.SkypeHolder;
 
 import java.io.Console;
 import java.io.IOException;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Main class for running bot
@@ -27,11 +30,58 @@ public class BirthDaySkypeBotRunner {
     private static ListenerRegister listenerRegister = new ListenerRegister();
 
     public static void main(String[] args) throws ConnectionException, ChatNotFoundException, ParseException, IOException, InvalidCredentialsException {
-        if (args.length > 0 && "contactsAdding".equals(args[0])) {
-            contactsAdding();
+        if (args.length > 0) {
+            String arg = args[0];
+            switch (arg) {
+                case "contactsAdding": {
+                    contactsAdding();
+                    break;
+                }
+                case "syncChats": {
+                    syncChats();
+                    break;
+                }
+                default: {
+                }
+            }
         } else {
             runMainLogic();
         }
+    }
+
+    private static void syncChats() throws ConnectionException, NotParticipatingException, InvalidCredentialsException {
+        initSkype();
+        final Skype skype = SkypeHolder.getSkype();
+        final Console console = System.console();
+        System.out.println("Enter count of chats:");
+        final Integer count = Integer.valueOf(console.readLine());
+        final Set<Chat> chats = new HashSet<Chat>(skype.loadMoreChats(count));
+        chats.addAll(skype.getAllChats());
+
+        System.out.println("Loaded " + chats.size() + " chats");
+
+        final Map<String, ContactWithBDay> allContactWithBDays = ContactRepository.getInstance().getAllContactWithBDays();
+        for (Chat chat : chats) {
+            if (chat instanceof GroupChat) {
+                final GroupChat groupChat = (GroupChat) chat;
+                final String topic = groupChat.getTopic();
+                System.out.println("Topic: " + topic);
+                System.out.println("Continue? y/n");
+                String ans = console.readLine();
+                if ("y".equals(ans)) {
+                    final ChatRepository chatRepository = ChatRepository.getInstance();
+                    for (ContactWithBDay contactWithBDay : allContactWithBDays.values()) {
+                        if (topic.contains(contactWithBDay.getTopicName())
+                                && !chatRepository.isChatExistForUser(contactWithBDay)) {
+                            final ChatForBDay chatForBDay = new ChatForBDay(groupChat, contactWithBDay);
+                            chatRepository.addChat(chatForBDay);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("syncChats finished");
     }
 
     private static void contactsAdding() throws NotParticipatingException, InvalidCredentialsException {
@@ -66,7 +116,7 @@ public class BirthDaySkypeBotRunner {
         System.out.println("Contact adding finished");
     }
 
-    private static void runMainLogic()  throws ConnectionException, ChatNotFoundException, InvalidCredentialsException, ParseException, IOException {
+    private static void runMainLogic() throws ConnectionException, ChatNotFoundException, InvalidCredentialsException, ParseException, IOException {
         initSkype();
         final Skype skype = SkypeHolder.getSkype();
         listenerRegister.process(skype);
